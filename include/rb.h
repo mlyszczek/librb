8245b7fa89e7389a011cafc252ef269f8fea5d6e
Michał Łyszczek <michal.lyszczek@bofc.pl>
2018-05-25 12:17:04 +0200

    add names to parameters in rb.h

--- include/rb.h
+++ include/rb.h
@@ -24,21 +24,23 @@
 #define O_MULTITHREAD 0x10000000L
 struct rb;
 
-struct rb *rb_new(size_t, size_t, unsigned long);
-struct rb *rb_init(size_t, size_t, unsigned long, void *);
-long rb_read(struct rb *, void *, size_t);
-long rb_recv(struct rb *, void *, size_t, unsigned long);
-long rb_write(struct rb *, const void *, size_t);
-long rb_send(struct rb *, const void *, size_t, unsigned long);
+struct rb *rb_new(size_t count, size_t object_size, unsigned long flags);
+struct rb *rb_init(size_t count, size_t object_size, unsigned long flags,
+    void *mem);
+long rb_read(struct rb *rb, void *buffer, size_t count);
+long rb_recv(struct rb *rb, void *buffer, size_t count, unsigned long flags);
+long rb_write(struct rb *rb, const void *buffer, size_t count);
+long rb_send(struct rb *rb, const void *buffer, size_t count,
+    unsigned long flags);
 
-int rb_clear(struct rb *, int);
-int rb_destroy(struct rb *);
-int rb_cleanup(struct rb *);
-int rb_stop(struct rb *);
-long rb_discard(struct rb *, size_t);
-const char *rb_version(char *, char *, char *);
-long rb_count(struct rb *);
-long rb_space(struct rb *);
+int rb_clear(struct rb *rb, int clear);
+int rb_destroy(struct rb *rb);
+int rb_cleanup(struct rb *rb);
+int rb_stop(struct rb *rb);
+long rb_discard(struct rb *rb, size_t count);
+const char *rb_version(char *major, char *minor, char *patch);
+long rb_count(struct rb *rb);
+long rb_space(struct rb *rb);
 size_t rb_header_size(void);
 
 #endif
